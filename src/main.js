c0c2d3637daa4db32f93a13ec238c483b9f4c65a
Alejandro Chacón <alexchaconvargas@gmail.com>
2016-06-19 18:27:02 +0200

    modularizing

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-//import Hello from './components/hello.js';
+import Hello from './components/hello.js';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
  
 import {Router, Route, hashHistory} from 'react-router';
-
+/*
 export default class Hello extends React.Component {
   render() {
     return <h1>Hello</h1>
   }
-}
+}*/
 
 ReactDOM.render(<Hello/>, document.getElementById('hello'));
