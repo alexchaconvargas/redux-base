7ef89d4464bebf9fe6a8f8bbf0ed0e3635465a0c
Alejandro Chacón <alexchaconvargas@gmail.com>
2016-06-22 17:12:16 +0200

    basic css layout

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -5,17 +5,21 @@ import layout from './home.css'
 export default class Home extends React.Component {
   render() {
     return (
+        <div>
     	<div className={layout.global}>
-    	<img src="/img/logo.png" />
-    	<h1> Dixtri Jobs </h1>
-    	<CoolButton text='A super cool button' />
-    	<ul>TODO - LIST
-		  <li>Basic CSS layout</li>
-		  <li>Testing</li>
-		  <li>Basic navigation</li>
-		  <li>Action example </li>
-		  <li>Saga example </li>
-		</ul>
+        	<img className={layout.brand} src="/img/logo.png" />
+            <h2 className={layout.logo}>Dixtri Jobs</h2>
+        	<CoolButton text='Acceso empresas'  />
+            <CoolButton text='Ofertas de empleo' />
+            <CoolButton text='Acceso candidatos' />
+        </div>
+        	<ul>TODO - LIST
+    		  <li>Basic CSS layout</li>
+    		  <li>Testing</li>
+    		  <li>Basic navigation</li>
+    		  <li>Action example </li>
+    		  <li>Saga example </li>
+    		</ul>
     	</div>
     	);
   }
---
+++ test/views/home_spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  renderIntoDocument
+} from 'react-addons-test-utils';
+import Home from '../../src/views/home';
+
+describe('Home', () => {
+
+  
+
+});
